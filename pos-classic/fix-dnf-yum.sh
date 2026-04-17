7cefc1a8cd5bd0b3c88e3dca517cfe14f78c0583
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2026-04-17 12:33:52 -0400

    updates

--- pos-classic/fix-dnf-yum.sh
+++ pos-classic/fix-dnf-yum.sh
@@ -1,3 +1,3 @@
-sudo dnf install https://yum.oracle.com/repo/OracleLinux/OL8/baseos/latest/x86_64/getPackage/oraclelinux-release-el8-1.0-35.el8.x86_64.rpm
+sudo dnf install -y https://yum.oracle.com/repo/OracleLinux/OL8/baseos/latest/x86_64/getPackage/oraclelinux-release-el8-1.0-35.el8.x86_64.rpm
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo /usr/bin/crb enable
