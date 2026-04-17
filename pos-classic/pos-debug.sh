33be6c379e6e81e08c88a591e34d3d2a6da50b63
Chad Lauritsen <chad.s.lauritsen@sherwin.com>
2026-04-17 12:59:50 -0400

    add debug script

---
+++ pos-classic/pos-debug.sh
@@ -0,0 +1,5 @@
+# I enabled the debugger on lb5200, to run it:
+# As user scs
+cd /fiscal
+mpro swstore -debug
+# Debugger window should open, select open>system/startup.p and then debug>run to run POS
